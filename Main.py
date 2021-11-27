cd97f00bf22e4c373099d4b1c68c5e6147a21999
Dmitry Moskalev <Diman1997qq@gmail.com>
2021-11-28 00:46:51 +0700

    [1.0] Code update

--- Main.py
+++ Main.py
@@ -533,7 +533,7 @@ class ProjectApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Green"
-        return Builder.load_file('main.kv')
+        return Builder.load_file('Main.kv')
 
 
 ProjectApp().run()
