7cb5728cf9dc80a88fb9dac08781ded41c99b5b6
Dmitry Moskalev <Diman1997qq@gmail.com>
2022-03-16 14:34:10 +0700

    [1.0] Code updates

--- main.py
+++ main.py
@@ -705,7 +705,6 @@ class Student_Digitizer(MDApp):
             self.root.current = 'menu'
 
     def build(self):
-        #self.theme_cls.primary_palette = "Green"
         return Builder.load_file('main.kv')
 
 
