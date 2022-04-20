220fedde00d961126eaea7d3bf4de40fc6f748aa
Dmitry <44023937+EnterSub@users.noreply.github.com>
2022-04-20 15:33:20 +0700

    Create MainActivity.kt

---
+++ kotlin/MainActivity.kt
@@ -0,0 +1,82 @@
+/* The fundamental package from The TensorFlow Authors */
+package org.tensorflow.lite.codelabs.digitclassifier
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import com.divyanshu.draw.widget.DrawView
+
+class MainActivity : AppCompatActivity() {
+  private var drawView: DrawView? = null
+  private var clearButton: Button? = null
+  private var predictedTextView: TextView? = null
+  private var symbolClassifier = SymbolDigitizer(this)
+
+  @SuppressLint("ClickableViewAccessibility")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // Setup view instances.
+    drawView = findViewById(R.id.draw_view)
+    drawView?.setStrokeWidth(70.0f)
+    drawView?.setColor(Color.WHITE)
+    drawView?.setBackgroundColor(Color.BLACK)
+    clearButton = findViewById(R.id.clear_button)
+    predictedTextView = findViewById(R.id.predicted_text)
+
+    // Setup clear drawing button.
+    clearButton?.setOnClickListener {
+      drawView?.clearCanvas()
+      predictedTextView?.text = getString(R.string.prediction_text_placeholder)
+    }
+
+    // Setup classification trigger so that it classify after every stroke drew.
+    drawView?.setOnTouchListener { _, event ->
+      drawView?.onTouchEvent(event)
+
+      // Then if user finished a touch event, run classification
+      if (event.action == MotionEvent.ACTION_UP) {
+        classifyDrawing()
+      }
+
+      true
+    }
+
+    // Setup digit classifier.
+    symbolClassifier
+      .initialize()
+      .addOnFailureListener { e -> Log.e(TAG, "Error to setting up symbol classifier.", e) }
+  }
+
+  override fun onDestroy() {
+    symbolClassifier.close()
+    super.
+    onDestroy()
+  }
+
+  private fun classifyDrawing() {
+    val bitmap = drawView?.getBitmap()
+
+    if ((bitmap != null) && (symbolClassifier.isInitialized)) {
+      symbolClassifier
+        .classifyAsync(bitmap)
+        .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
+        .addOnFailureListener { e ->
+          predictedTextView?.text = getString(
+            R.string.classification_error_message,
+            e.localizedMessage
+          )
+          Log.e(TAG, "Error in classifying drawing.", e)
+        }
+    }
+  }
+
+  companion object {
+    private const val TAG = "MainActivity"
+  }
+}
