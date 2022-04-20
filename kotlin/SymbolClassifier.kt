c50df7928ae8a57f2eef764983e2541ca576fde3
Dmitry <44023937+EnterSub@users.noreply.github.com>
2022-04-20 15:38:49 +0700

    Update SymbolClassifier.kt

--- kotlin/SymbolClassifier.kt
+++ kotlin/SymbolClassifier.kt
@@ -21,7 +21,6 @@ class SymbolDigitizer(private val context: Context) {
     private set
 
   private val executorService: ExecutorService = Executors.newCachedThreadPool()
-
   private var inputImageWidth: Int = 0
   private var inputImageHeight: Int = 0
   private var modelInputSize: Int = 0
@@ -46,17 +45,14 @@ class SymbolDigitizer(private val context: Context) {
     val options = Interpreter.Options()
     options.setUseNNAPI(true)
     val interpreter = Interpreter(model, options)
-
     // Read input shape from model file.
     val inputShape = interpreter.getInputTensor(0).shape()
     inputImageWidth = inputShape[1]
     inputImageHeight = inputShape[2]
     modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth *
       inputImageHeight * PIXEL_SIZE
-
     // Finish interpreter initialization.
     this.interpreter = interpreter
-
     isInitialized = true
     Log.d(TAG, "Initialized TFLite interpreter.")
   }
@@ -82,15 +78,11 @@ class SymbolDigitizer(private val context: Context) {
       true
     )
     val byteBuffer = convertBitmapToByteBuffer(resizedImage)
-
     // Define an array to store the model output.
     val output = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
-
     // Run inference with the input data.
     interpreter?.run(byteBuffer, output)
-
-    // Post-processing: find the digit that has the highest probability
-    // and return it a human-readable string.
+    // Post-processing: find the digit that has the highest probability like string.
     val result = output[0]
     val maxIndex = result.indices.maxByOrNull { result[it] } ?: -1
     val resultString =
@@ -119,7 +111,6 @@ class SymbolDigitizer(private val context: Context) {
   private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
     val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
     byteBuffer.order(ByteOrder.nativeOrder())
-
     val pixels = IntArray(inputImageWidth * inputImageHeight)
     bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
 
@@ -127,21 +118,17 @@ class SymbolDigitizer(private val context: Context) {
       val r = (pixelValue shr 16 and 0xFF)
       val g = (pixelValue shr 8 and 0xFF)
       val b = (pixelValue and 0xFF)
-
       // Convert RGB to grayscale and normalize pixel value to [0..1].
       val normalizedPixelValue = (r + g + b) / 3.0f / 255.0f
       byteBuffer.putFloat(normalizedPixelValue)
     }
-
     return byteBuffer
   }
 
   companion object {
     private const val TAG = "SymbolClassifier"
-
     private const val FLOAT_TYPE_SIZE = 4
     private const val PIXEL_SIZE = 1
-
     private const val OUTPUT_CLASSES_COUNT = 10 //Currently for digits
   }
 }
